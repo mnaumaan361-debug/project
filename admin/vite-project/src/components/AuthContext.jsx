bf1d000a827252aa28335d9a5c5cfad79b49a02c
mnaumaan361-debug <mnaumaan361@gmail.com>
2026-08-06 12:20:34 +0530

    Update AuthContext.jsx

--- admin/vite-project/src/components/AuthContext.jsx
+++ admin/vite-project/src/components/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 
 export const AuthDataContext=createContext()
 function AuthContext({children}) {
-    let serverUrl="http://localhost:8000/"
+    let serverUrl="https://onecart-backend-mwjo.onrender.com"
     let value={
         serverUrl,
     }
