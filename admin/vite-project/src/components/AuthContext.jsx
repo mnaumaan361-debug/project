ce11d7b49cabab102bf4f8e997e55f2a6e496233
mnaumaan361-debug <mnaumaan361@gmail.com>
2026-08-07 00:49:51 +0530

    Remove node_modules from admin

--- admin/vite-project/src/components/AuthContext.jsx
+++ admin/vite-project/src/components/AuthContext.jsx
@@ -3,7 +3,11 @@ import { createContext } from 'react'
 
 export const AuthDataContext=createContext()
 function AuthContext({children}) {
+<<<<<<< HEAD
     let serverUrl = "https://onecart-backend-mwjo.onrender.com/"
+=======
+let serverUrl = "https://onecart-backend-mwjo.onrender.com/"
+>>>>>>> fa7ae8e (Ignore dist)
     let value={
         serverUrl,
     }
