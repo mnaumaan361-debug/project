212328bb323229c461426cb803ea69e10c2994ef
mnaumaan361-debug <mnaumaan361@gmail.com>
2026-08-07 10:48:19 +0530

    Update backend production URL

--- frontend/my-app/src/context/AuthContext.jsx
+++ frontend/my-app/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
  export const authDataContext=createContext()
 
 function AuthContext({children}) {
-    let serverUrl="https://onecart-backend-mwjo.onrender.com"
+      let serverUrl = "https://onecart-backend-mwjo.onrender.com"
     let value={
 serverUrl
     }
